0da5ad8261342ac37cc927258891c1e1b224d314
kerzhao <kerzhao@163.com>
2018-06-13 20:17:03 +0800

    update

--- adam/backtesting/turtle.py
+++ adam/backtesting/turtle.py
@@ -5,3 +5,10 @@ import numpy as np
 import pandas as pd
 import redis
 
+class ClassicTurtle(object):
+    
+    def __init__(self):
+        pass
+    
+    def get_N(self, p):
+        
--- adam/backtesting/turtle_test.py
+++ adam/backtesting/turtle_test.py
@@ -1,6 +1,7 @@
 #encoding:UTF-8
 
 import os
+import sys
 import numpy as np
 import pandas as pd
 import redis
@@ -12,22 +13,36 @@ from jaqs.data import DataApi
 import turtle
 
 class TurtleTest(unittest.TestCase):
+    user_id = ''
     
     def setUp(self):
-        api = DataApi(addr='tcp://data.tushare.org:8910')
-        api.login("1881~", "eyJhbGciOiJIUzI1NiJ9.eyJjcmVhdGVfdGltZSI6IjE1MjY5MDQ0NTgwODAiLCJpc3MiOiJhdXRoMCIsImlkIjoiMTg4MTAyOTkyMDAifQ.M9d07UvpEWVWugga0d2UtbWsRRZubS5mgMXaTxrieSk")
+        self.api = DataApi(addr='tcp://data.tushare.org:8910')
+        self.api.login(self.user_id, "eyJhbGciOiJIUzI1NiJ9.eyJjcmVhdGVfdGltZSI6IjE1MjY5MDQ0NTgwODAiLCJpc3MiOiJhdXRoMCIsImlkIjoiMTg4MTAyOTkyMDAifQ.M9d07UvpEWVWugga0d2UtbWsRRZubS5mgMXaTxrieSk")
     
     def tearDown(self):
         pass
     
-    def test_0(self):
-        pass
+    def test_api(self):
+        df, msg = self.api.daily(
+            symbol="cu1801.SHF",
+            start_date="2017-10-26",
+            end_date="2018-11-06",
+            fields="",
+            adjust_mode="post")
+        self.assertEqual(msg, '0,')
+        self.assertTupleEqual(df.shape, (57, 16))
     
 
 def suite():
     suite = unittest.TestSuite()
-    suite.addTest(TurtleTest('test_0'))
+    suite.addTest(TurtleTest('test_api'))
     return suite
 
 if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print('***Must set the user id,\n'
+              '***e.g\n'
+              '***python turtle_test.py 18812345678')
+    else:
+        TurtleTest.user_id = sys.argv.pop()
     unittest.main(defaultTest='suite')
